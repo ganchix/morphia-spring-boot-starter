6db431d9baef9c262ca6950447e072ad89de9560
Rafael Ríos Moya <rafael@fintonic.com>
2017-05-21 22:43:43 +0200

    Add tests

---
+++ morphia-spring-boot/src/test/java/io/ganchix/github/morphia/utils/MorphiaUtilsTest.java
@@ -0,0 +1,48 @@
+package io.ganchix.github.morphia.utils;
+
+import io.ganchix.github.morphia.utils.context.error.SpringApplicationAcceptNoSpringBootApplication;
+import io.ganchix.github.morphia.utils.context.normal.SpringApplicationTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by Rafael Ríos on 5/05/17.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class MorphiaUtilsTest {
+
+    @Test(expected = Exception.class)
+    public void testWhenNoApplicationAnnotation_ShouldReturnAException() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringApplicationAcceptNoSpringBootApplication.class);
+        MorphiaUtils.getApplicationPackageName(context);
+    }
+
+    @Test
+    public void testCallGetPackageName_ShouldReturnAPackageName() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringApplicationTest.class);
+        List<String> applicationPackageName = MorphiaUtils.getApplicationPackageName(context);
+        assertThat(applicationPackageName.size(), is(1));
+        assertThat(applicationPackageName.get(0), is("es.ganchix.morphia.utils.context.normal"));
+    }
+
+    @Test
+    public void testCallGetClasses_ShouldReturnAClassesInPackage() {
+        Set<Class<?>> classes = MorphiaUtils.getClasses("es.ganchix.morphia.utils.context.normal");
+        assertThat(classes.size(), is(2));
+
+    }
+    @Test
+    public void testCallGetClasses_ShouldReturnAClassesInPackage1() {
+        Set<Class<?>> classes = MorphiaUtils.getClasses("es.ganchix.morphia.utils.context.normal.NotClass.class");
+    }
+
+
+}
